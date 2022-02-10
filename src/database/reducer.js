09f1345e42bce208cd20f9b7dfb2efd4bd4958de
Serrjik <uac.sarge@gmail.com>
2022-02-10 15:35:55 +0300

    Добавлено сохранение состояния в LS при создании нового заказа

--- src/database/reducer.js
+++ src/database/reducer.js
@@ -75,23 +75,20 @@ export default function reducer(state, action) {
 	if (action.type === CREATE_ORDER) {
 		const id = 1 + Math.max(0, ...state.orders.map(x => x.id))
 
-		// localStorage.setItem(
-		// 	'CRM_REACT_ORDERS',
-		// 	JSON.stringify({
-		// 		...state,
-		// 		orders: [...state.orders, { id, ...action.payload }],
-		// 	})
-		// )
-		// console.log('state.orders: ', state.orders)
+		localStorage.setItem(
+			'CRM_REACT_ORDERS',
+			JSON.stringify({
+				...state,
+				orders: [...state.orders, { id, ...action.payload }],
+			})
+		)
+		console.log('state.orders: ', state.orders)
 
 		return {
 			...state,
 			orders: [...state.orders, { id, ...action.payload }],
 		}
 	}
-
-	// localStorage.setItem('CRM_REACT_ORDERS', JSON.stringify(state.orders))
-	// console.log('state.orders: ', state.orders)
 
 	return { ...state }
 }
