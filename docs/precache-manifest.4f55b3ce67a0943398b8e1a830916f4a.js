03883dd974a15dda72cc87e359b01070eebc073a
Serrjik <uac.sarge@gmail.com>
2022-02-11 16:09:38 +0300

    docs folder added

---
+++ docs/precache-manifest.4f55b3ce67a0943398b8e1a830916f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b1997d73c414bcfee236242a0370c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ececa9ac83410abb44a",
+    "url": "/static/css/main.2399c29f.chunk.css"
+  },
+  {
+    "revision": "a26fca8ea41bed494d42",
+    "url": "/static/js/2.d09fd73b.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/static/js/2.d09fd73b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ececa9ac83410abb44a",
+    "url": "/static/js/main.976c4467.chunk.js"
+  },
+  {
+    "revision": "38d5d961dcf5937ee6c4",
+    "url": "/static/js/runtime-main.a55c223a.js"
+  }
+]);
